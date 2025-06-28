812fcccb49ea93798a8c68a2516fe915833a530a
Your Name <bubbaman07.ek@gmail.com>
2025-06-27 20:27:09 -0400

    Added Set functions and saving for settings

--- Application Base/settings.h
+++ Application Base/settings.h
@@ -6,15 +6,23 @@
 
 class Settings {
 public:
-	static void Load(const std::string& filepath);
+    static void Load(const std::string& filepath);
+    static void Save(const std::string& filepath);  // Save to a specific file
 
-	static bool GetBool(const std::string& key);
-	static int GetInt(const std::string& key);
-	static float GetFloat(const std::string& key);
-	static std::string GetString(const std::string& key);
+    // Setters
+    static void Set(const std::string& key, const std::string& value);
+    static void Set(const std::string& key, bool value);
+    static void Set(const std::string& key, int value);
+    static void Set(const std::string& key, float value);
+
+    // Getters
+    static bool GetBool(const std::string& key);
+    static int GetInt(const std::string& key);
+    static float GetFloat(const std::string& key);
+    static std::string GetString(const std::string& key);
 
 private:
-	static std::unordered_map<std::string, std::string> settings;
+    static std::unordered_map<std::string, std::string> settings;
 };
 
 #endif
